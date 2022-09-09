d5b60e0bda8d8cc231f82e060789b298d4419a00
Ibtisam Mirza <iuhmirza@gmail.com>
2022-09-09 12:10:58 +0100

    Add post method to person

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -51,6 +53,28 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name && body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const person = {
+        id: Math.floor(Math.random()*100+10),
+        name: body.name,
+        number: body.number
+
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+
 })
 
 const PORT = 3001
