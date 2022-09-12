0d24d0a8c3735b144f914f19e6f1c0377989225a
Ibtisam Mirza <iuhmirza@gmail.com>
2022-09-12 15:19:19 +0100

    Improve error handling

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if(!body.name && body.number) {
@@ -61,6 +61,7 @@ app.post('/api/persons', (request, response) => {
     })
 
     person.save().then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 
 })
 
@@ -88,6 +89,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({error: 'malformed id'})
+    }else if(error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
--- models/phonebook.js
+++ models/phonebook.js
@@ -9,8 +9,15 @@ mongoose.connect(url)
     .catch((error) => console.log('error connecting to mongoDB', error.message))
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      required: true,
+    },
     date: Date,
 })
 
